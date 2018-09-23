15f33ae4a4df167614e25a3d0131235a68ab9ac3
emil.huo <emil.huo@gmail.com>
2018-09-23 11:31:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var katzDeli = [];
 function takeANumber(number, name) {
 katzDeli.push(`${name}`);
 
-return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
+return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 
 }
