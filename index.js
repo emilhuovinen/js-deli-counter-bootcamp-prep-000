4446f7ee2b0724f058c92f2a0b34fdd77c7f0f77
emil.huo <emil.huo@gmail.com>
2018-09-23 12:10:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ function currentLine(katzDeli) {
   while (i < katzDeli.length) {
     line.push(` `+[i+1]+`. `  + katzDeli[i])
     i++
-    
+  }
     if (katzDeli.length === 0) {
       return `The line is currently empty.`;
     } else {
       return (`The line is currently:` + line );
-    }
+    } }
