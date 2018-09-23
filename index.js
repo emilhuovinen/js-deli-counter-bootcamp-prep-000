2eae59ada3b7e17228b636e3b8a1839bf95caca6
emil.huo <emil.huo@gmail.com>
2018-09-23 11:36:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 
 var katzDeli = [];
+var katzDeliLine = [];
 
 function takeANumber(katzDeli, name) {
 katzDeli.push(`${name}`);
 
 return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 
+}
+
+function nowServing(katzDeliLine) {
+  
+  return katzDeli[0];
+  
 }
